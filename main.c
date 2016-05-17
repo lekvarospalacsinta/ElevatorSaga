dd360742124b154b0cc8d501c74129b6e46760f4
Marton <biro.marton@gmail.com>
2016-05-17 17:44:46 +0300

    optimization on indicator lights

--- main.c
+++ main.c
@@ -6,25 +6,56 @@
         {
             pressedup[i] = 0;
             presseddw[i] = 0;
-            floors[i].on("up_button_pressed", function() { sendFreestElevator(this.floorNum(), true); });
-            floors[i].on("down_button_pressed", function() { sendFreestElevator(this.floorNum(), false); });
+            floors[i].on("up_button_pressed", function() {
+                var j = 0;
+                var k = elevators[0].loadFactor();
+                //console.log(0, "with factor" ,elevators[0].loadFactor());
+                for (var i = 1; i < elevators.length; i++){
+                    //console.log(i, "with factor" ,elevators[i].loadFactor());
+                    if (elevators[i].loadFactor() < k) {
+                        k = elevators[i].loadFactor();
+                        j = i;
+                    }
+                }
+                //console.log(j, "choosen");
+                addFloorToElevator(elevators[j], this.floorNum());
+                pressedup[this.floorNum()] += 1;
+                //console.log("uppressed",this.floorNum());
+            });
+            floors[i].on("down_button_pressed", function() {
+                var j = 0;
+                var k = elevators[0].loadFactor();
+                //console.log(0, "with factor" ,elevators[0].loadFactor());
+                for (var i = 1; i < elevators.length; i++){
+                    //console.log(i, "with factor" ,elevators[i].loadFactor());
+                    if (elevators[i].loadFactor() < k) {
+                        k = elevators[i].loadFactor();
+                        j = i;
+                    }
+                }
+                //console.log(j, "choosen");
+                addFloorToElevator(elevators[j], this.floorNum());
+                presseddw[this.floorNum()] += 1;
+                //console.log("dwpressed",this.floorNum());
+            });
         }
         for (var i = 0; i < elevators.length; i++)
         {
             var elevator = elevators[i];
-            elevator.on("idle", function()                         { addFloorToElevator(elevator, 0); });
-            elevator.on("floor_button_pressed", function(floorNum) { addFloorToElevator(elevator, floorNum); });
+            elevator.on("idle", function()                         { addFloorToElevator(this, 0); });
+            elevator.on("floor_button_pressed", function(floorNum) { addFloorToElevator(this, floorNum); });
             elevator.on("passing_floor", function(floorNum, direction) {
-                stopAtFloor(elevator, floorNum, direction);
+                stopAtFloor(this, floorNum, direction);
             });
             elevator.on("stopped_at_floor", function(floorNum) {
-                removeFloorFromElevator(elevator, floorNum);
+                removeFloorFromElevator(this, floorNum);
             });
         }
+        
         function stopAtFloor(elevator, floorNum, direction)
         {
             //console.log("load",elevator.loadFactor(),"before floor",floorNum,"direction", direction, "dw", presseddw, "up", pressedup);
-            if (elevator.loadFactor() < 1) {
+            if (elevator.loadFactor() < 0.5) {
                 if (((presseddw[floorNum] > 0) && (direction === "down")) || ((pressedup[floorNum] > 0) && (direction === "up"))) {
                     //console.log("stopping");
                     elevator.destinationQueue.unshift(floorNum);
@@ -33,40 +64,19 @@
             }
         }
 
-        function sendFreestElevator(floorNum, direction) {
-            j = 0;
-            k = 0;
-            for (var i = 0; i < elevators.length; i++){
-                if (elevators[i].loadfactor > k){
-                    k = elevator[i].loadFactor();
-                    j = i;
-                }
-            }
-            addFloorToElevator(elevators[j], floorNum);
-            if (direction === true) {
-                pressedup[floorNum] += 1;
-                //console.log("uppressed",floorNum);
-            }
-            else {
-                presseddw[floorNum] += 1;
-                //console.log("downpressed",floorNum);
-            }
-        }
-
         function addFloorToElevator(elevator, floorNum) {
             var seen = false;
             for (var i = 0; i < elevator.destinationQueue.length; i++) {
-                var item = elevator.destinationQueue[i];
                 if (elevator.destinationQueue[i] === floorNum) {
                     seen = true;
-                    console.log(floorNum, "already there");
+                    //console.log(floorNum, "already there");
                 }
             }
             if (seen !== true) {
                 elevator.goToFloor(floorNum);
                 //elevator.destinationQueue.sort();
                 //elevator.checkDestinationQueue();
-                console.log("add", elevator.destinationQueue);
+                //console.log("add", elevator.destinationQueue);
             }
         }
 
@@ -76,7 +86,7 @@
             for (var i = 0; i < elevator.destinationQueue.length; i++) {
                 if (elevator.destinationQueue[i] === floorNum) {
                     elevator.destinationQueue.splice(i--);
-                    console.log(floorNum, "deleted duplicate");
+                    //console.log(floorNum, "deleted duplicate");
                 }
             }
             elevator.checkDestinationQueue();
@@ -88,28 +98,32 @@
             {
                 presseddw[floorNum] = 0;
             }
-            console.log("remove", floorNum, elevator.destinationQueue);
+            //console.log("remove", floorNum, elevator.destinationQueue);
         }
     },
         update: function(dt, elevators, floors) {
-            if ((elevators[0].destinationQueue[0] > elevators[0].currentFloor()) || elevators[0].currentFloor() === 0) {
-                elevators[0].goingUpIndicator(true);                
-                elevators[0].goingDownIndicator(false);
-            }
-            else {
-                if ((elevators[0].destinationQueue[0] < elevators[0].currentFloor()) || elevators[0].currentFloor() === floors.length) {
-                    elevators[0].goingDownIndicator(true);
-                    elevators[0].goingUpIndicator(false);                
-                }
-                else {
-                    if (elevators[0].destinationQueue[1] < elevators[0].currentFloor()) {
-                        elevators[0].goingDownIndicator(true);
-                        elevators[0].goingUpIndicator(false);                
-                    }
-                    else if (elevators[0].destinationQueue[1] > elevators[0].currentFloor()) {
-                        elevators[0].goingUpIndicator(true);                
-                        elevators[0].goingDownIndicator(false);
-                    }
+            for (var i = 0; i < elevators.length; i++) {
+                if (elevators[i].currentFloor() < 1) {
+                    elevators[i].goingUpIndicator(true);                
+                    elevators[i].goingDownIndicator(false);
+                } else if (elevators[i].currentFloor() > (floors.length - 2)) {
+                    elevators[i].goingDownIndicator(true);
+                    elevators[i].goingUpIndicator(false);                
+                } else if ((elevators[i].destinationQueue[0] > elevators[i].currentFloor()) && (elevators[i].destinationQueue.length > 0)) {
+                    elevators[i].goingUpIndicator(true);                
+                    elevators[i].goingDownIndicator(false);
+                } else if ((elevators[i].destinationQueue[0] < elevators[i].currentFloor()) && (elevators[i].destinationQueue.length > 0)) {
+                    elevators[i].goingDownIndicator(true);
+                    elevators[i].goingUpIndicator(false);                
+                } else if ((elevators[i].destinationQueue[1] > elevators[i].currentFloor()) && (elevators[i].destinationQueue.length > 1)) {
+                    elevators[i].goingUpIndicator(true);                
+                    elevators[i].goingDownIndicator(false);
+                } else if ((elevators[i].destinationQueue[1] < elevators[i].currentFloor()) && (elevators[i].destinationQueue.length > 1)) {
+                    elevators[i].goingDownIndicator(true);
+                    elevators[i].goingUpIndicator(false);
+                } else {
+                    elevators[i].goingDownIndicator(true);
+                    elevators[i].goingUpIndicator(true);
                 }
             }
         }
